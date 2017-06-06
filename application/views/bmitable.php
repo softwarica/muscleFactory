9dae0b5f30392a595ffe02a231614068333b711d
harisharan shrestha <sharan.softwarica@gmail.com>
2017-06-06 09:41:51 +0545

    change on bmitable

--- application/views/bmitable.php
+++ application/views/bmitable.php
@@ -31,54 +31,169 @@
 									<td>181</td><td>186</td><td>191</td>
 					</tr>
 					<tr>
-									<td>94</td>
-									<td>	99</td>	104	109	114	119	124	128	133	138	143	148	153	158	163	168	173
+						<th>59</th>	<td>94</td>
+									<td>99</td>
+									<td>104</td>
+									<td>109</td>
+								<td>114</td><td>119</td><td>124</td><td>128</td><td>133</td><td>138</td>
+								<td>143</td><td>148</td><td>153</td><td>158</td><td>163</td><td>168</td><td>173</td>
+								<td>178</td><td>183</td><td>188</td><td>193</td><td>198</td>
 					</tr>
 					<tr>
-						<th>60</th>
+						<th>60</th><td>97</td>	
+						<td>102</td>
+						<td>107</td>
+						<td>112</td>
+						<td>118</td>	
+						<td>123</td><td>128</td><td>133</td><td>138</td><td>143</td><td>148</td><td>153</td><td>158</td>
+						<td>163</td><td>168</td><td>174</td><td>179</td><td>184</td>	<td>189</td><td>194</td><td>199</td>
+						<td>	204</td>
 					</tr>
 					<tr>
-						<th>61</th>
+						<th>61</th><td>100</td>
+						<td>	106</td>
+						<td>	111</td>
+						<td>	116</td>
+						<td>	122</td>
+						<td>	127</td>
+						<td>	132</td>
+						<td>	137</td>
+						<td>	143</td>
+						<td>	148</td>
+						<td>	153</td>
+						<td>	158</td>
+						<td>	164</td>
+						<td>	169</td>
+							<td>174</td>
+							<td>	180</td>
+							<td>	185</td><td>190</td><td>	195</td><td>	201</td><td>	206</td><td>	211</td>
 					</tr>
 					<tr>
-						<th>62</th>
+						<th>62</th><td>104</td>
+						<td>	109</td>
+						<td>	115</td>
+						<td>	120</td>
+						<td>	126</td>
+						<td>	131</td>
+						<td>	136</td>
+						<td>	142</td>
+						<td>	147</td>
+						<td>	153</td>
+						<td>	158</td>
+						<td>	164</td>
+						<td>	169</td>
+						<td>	175</td>
+						<td>	180</td>
+						<td>	186</td>
+						<td>	191</td>
+						<td>196</td><td>	202</td><td>	207</td><td>	213</td><td>	218</td>
 					</tr>
 					<tr>
-						<th>63</th>
+						<th>63</th><td>107</td><td>	113</td><td>118</td>
+						<td>	124</td><td>	130</td><td>	135</td>
+						<td>	141</td><td>	146</td><td>	152</td><td>	158</td>
+						<td>	163</td><td>	169</td><td>	175</td><td>	180</td><td>186</td>
+						<td>	191</td><td>	197</td>
+						<td>203</td>
+						<td>	208</td>
+						<td>	214</td>
+						<td>	220</td>
+						<td>	225</td>
+						
+
 					</tr>
 					<tr>
-						<th>64</th>
+						<th>64</th><td>110</td>	<td>116</td>	<td>122</td>	<td>128</td><td>	134</td><td>	140</td>
+						<td>	145</td>
+						<td>	151</td><td>	157</td><td>	163</td>
+						<td>	169</td>
+						<td>	174</td><td>	180</td><td>	186</td><td>	192</td><td>	197</td>
+						<td>	204</td>
+						<td>209</td>
+						<td>	215</td>
+						<td>	221</td>
+
+						<td>	227</td>
+						<td>	232</td>
+						
+
+
 					</tr>
 					<tr>
-						<th>65</th>
+						<th>65</th><td>114</td>
+						<td>	120</td>
+						<td>	126</td>
+						<td>	132</td>
+						<td>	138</td>
+						<td>	144</td>
+						<td>	150</td>
+						<td>	156</td>
+						<td>	162</td>
+						<td>	168</td>
+						<td>	174</td>
+						<td>	180</td>
+						<td>	186</td>
+						<td>	192</td>
+						<td>	198</td>
+						<td>	204</td>
+						<td>	210</td><td>216</td>
+						<td>	222</td>
+						<td>	228</td>
+						<td>	234</td><td>	240</td>
+
 					</tr>
 					<tr>
 						<th>66</th>
+						<td>	118</td>
+						<td>	124</td>
+						<td>	130</td>
+						<td>	136</td>
+						<td>	142</td>
+						<td>	148</td>
+						<td>	155</td>
+						<td>	161</td>
+						<td>	167</td>
+						<td>	173</td>
+						<td>	179</td>
+						<td>	186</td>
+						<td>	192</td>
+						<td>	198</td>
+						<td>	204</td>
+						<td>	210</td>
+						<td>	216</td>
+						<td>223</td>
+						<td>	229</td>
+						<td>	235</td>
+						<td>	241</td>
+						<td>	247</td>
+
 					</tr>
 					<tr>
 						<th>67</th>
+						<td>	121</td>
+						<td>	127</td>
+						<td>	134</td>
+						<td>	140</td>
+						<td>	146</td>
+						<td>	153</td>
+						<td>	159</td>
+						<td>	166</td>
+						<td>	172</td>
+						<td>	178</td>
+						<td>	185</td>
+						<td>	191</td>
+						<td>	198</td>
+						<td>	204</td>
+						<td>	211</td>
+						<td>	217</td>
+						<td>	223</td>
+						<td>230</td>
+						<td>	236</td>
+						<td>	242</td>
+						<td>	249</td>
+						<td>	255</td>
 					</tr>
-					<tr>
-						<th>68</th>
-					</tr>
-					<tr>
-						<th>69</th>
-					</tr>
-					<tr>
-						<th>70</th>
-					</tr>
-					<tr>
-						<th>71</th>
-					</tr>
-					<tr>
-						<th>72</th>
-					</tr>
-					<tr>
-						<th>73</th>
-					</tr>
-					<tr>
-						<th>74</th>
-					</tr>
+					
 					</tbody>
 				</table>
 			</div>
